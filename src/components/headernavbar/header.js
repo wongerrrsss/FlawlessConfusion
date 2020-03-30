adaa4ea2d41c5386c71abc106fdc8393c54f6c3d
Tawonga Chilongo <tmchilongo92@gmail.com>
2020-03-30 12:13:21 -0400

    built out header and navbar

---
+++ src/components/headernavbar/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <img src="../../static/images/flawlessconfusionlogo.png" />
+            </div>
+        )
+    }
+}
+
+export default Header; 
---
+++ src/components/headernavbar/navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div className="navbar">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">clothing.</a>
+                        </li>
+                        <li>
+                            <a href="#">accessories.</a>
+                        </li>
+                        <li>
+                            <a href="#">sale</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Navbar; 
